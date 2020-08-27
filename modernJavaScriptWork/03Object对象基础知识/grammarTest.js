e214106efea8690bdc5162ef14bfd4cc65723c83
Disat <zxplayers@163.com>
2020-08-27 18:03:43 +0800

    进行到数据类型

--- modernJavaScriptWork/03Object对象基础知识/grammarTest.js
+++ modernJavaScriptWork/03Object对象基础知识/grammarTest.js
@@ -4,21 +4,97 @@
 // user["name"]=Pete;
 // delete user["name"]
 
-let calculator = {
-  sum() {
-    return a + b;
-  },
+// let calculator = {
+//   sum() {
+//     return a + b;
+//   },
 
-  mul() {
-    return a * b;
-  },
+//   mul() {
+//     return a * b;
+//   },
 
-  read() {
-    a = +prompt('a?', 0);
-    b = +prompt('b?', 0);
-  }
-};
+//   read() {
+//     a = +prompt('a?', 0);
+//     b = +prompt('b?', 0);
+//   }
+// };
 
-calculator.read();
-alert( calculator.sum() );
-alert( calculator.mul() );
+// calculator.read();
+// console.log( calculator.sum() );
+// console.log( calculator.mul() );
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// let obj = {}
+
+// function A(){
+//   let a = 1
+//   return obj
+// }
+
+// function B(){
+//   let b =2
+//   return obj
+// }
+
+// let a = new A;
+// let b = new B;
+
+// console.log(a,b);
+// console.log(a == b);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Accumulator(startValue){
+  
+//   this.read(){
+//     userInput = prompt('please input');
+//   }
+
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// let user={
+//   name:'John'
+// };
+
+// let id = Symbol('id')
+// user[id] = 1;
+// console.log(user[id]);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// let id = Symbol("id")
+// let user = {
+//   [id]:23,
+//   id:"123"
+
+// }
+// let admin ={}
+
+// Object.assign(admin,user)
+
+// console.log(admin.id);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// console.log('1');
+// console.log('2');
+// console.log(__filename);
+// console.log(__dirname);
+// console.log(module);
+// console.log(require);
+// console.log(module);
+// console.log(exports);
+// console.log(this);
+// console.log();
+let user = {name: "John"};
+
+let s = String.toString(user);
+
+let a = user.valueOf()
+console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
